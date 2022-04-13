3149a4a0b6baa964a3ef7080d10a8ffd57d7874c
Kateřina Petrášková <ka.petraskova@gmail.com>
2022-04-13 20:08:44 +0200

    zelvicka nakreslí 4 domecky

--- src/main/java/net/sevecek/turtle/HlavniProgram.java
+++ src/main/java/net/sevecek/turtle/HlavniProgram.java
@@ -8,7 +8,29 @@ public class HlavniProgram {
         Turtle zofka;
 
         zofka = new Turtle();
-        zofka.move(100);
+        zofka.setPenWidth(5);
+        zofka.penUp();
+        zofka.turnLeft(90);
+        zofka.move(350);
+        zofka.penDown();
+        zofka.turnRight(90);
+
+        for (int j = 0; j < 4; j++) {
+            for (int i = 0; i < 4; i++) {
+                zofka.turnRight(90);
+                zofka.move(100);
+            }
+            zofka.turnRight(45);
+            zofka.move(70.7106);
+            zofka.turnRight(90);
+            zofka.move(70.7106);
+            zofka.penUp();
+            zofka.turnLeft(45);
+            zofka.move(50);
+            zofka.turnLeft(90);
+            zofka.penDown();
+        }
+
     }
 
 }
